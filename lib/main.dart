22f2c2791d80e4410d093a778c4ca47954755e3b
ryamate <iiiiiii.satenanbon@gmail.com>
2023-02-03 17:44:29 +0900

    英語記事の内容に修正

--- lib/main.dart
+++ lib/main.dart
@@ -3,28 +3,40 @@ import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: WebViewApp(),
+    MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: const WebViewApp(),
     ),
   );
 }
 
 class WebViewApp extends StatefulWidget {
-  const WebViewApp({Key? key}) : super(key: key);
+  const WebViewApp({super.key});
 
   @override
   State<WebViewApp> createState() => _WebViewAppState();
 }
 
 class _WebViewAppState extends State<WebViewApp> {
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..loadRequest(
+        Uri.parse('https://flutter.dev'),
+      );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter WebView'),
       ),
-      body: const WebView(
-        initialUrl: 'https://flutter.dev',
+      body: WebViewWidget(
+        controller: controller,
       ),
     );
   }
